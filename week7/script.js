ea912fb7920d11cb688f0994b9bb9da4ad2d92c4
Gutierrez <tgutierrez@santarosa.edu>
2023-03-15 15:33:24 -0700

    Upload directories

---
+++ week7/script.js
@@ -0,0 +1,10 @@
+/* Students: Please use this week's project for Week 7: Assignment 7: jQuery Plugin Promotional Site. 
+     You will need to replace the contents of this JavaScript file with your own work, 
+     and create any other files, if any, required for the assignment.
+     When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+"use strict";
+let person = prompt("What's your name?");
+
+if (person != null) {
+   document.getElementById("hello").innerHTML += " " + person + "!";
+}
